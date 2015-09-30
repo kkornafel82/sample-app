27dd7dc49741ce5c01ba870945fabb7df4a7b65e
Kevin <kkornafel82@yahoo.com>
2015-09-29 19:14:10 -0700

    sample_branch4

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,6 +1,7 @@
 class PostController < ApplicationController
   
   def sample_method
+    returns "this is a sample method"
   end
   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+
+has_many :sample_users
+
 end
